478b5f0d0a3e18029e65907377430a1ed4d29869
MoonStar16 <umayaatika@gmail.com>
2021-11-22 13:58:52 +0700

    menambahkan pragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora, rerum nemo minima totam vel culpa. Exercitationem possimus commodi cumque reiciendis nisi sed? Magnam voluptates odio iste assumenda deserunt quas dolor!</p>
 </body>
 </html>
